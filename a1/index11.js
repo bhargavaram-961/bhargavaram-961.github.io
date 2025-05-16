05401cbefa22fe4bec35e573b208f246bb1d388e
bhargavaram-961 <bhargavaram961@gmail.com>
2025-05-16 19:31:41 +0530

    updated

---
+++ a1/index11.js
@@ -0,0 +1,7 @@
+const products =[
+    {id:1,name:"hello",price:34},
+    {id:1,name:"hello 1",price:34},
+    {id:1,name:"hello 2",price:34}
+
+];
+products.forEach((products)=>console.log(products.name));
