a0e20c6c187d6c575c918c415d7389488b549a2a
bhargavaram-961 <bhargavaram961@gmail.com>
2025-05-17 19:14:59 +0530

    updated

--- a1/index12.js
+++ a1/index12.js
@@ -1,10 +1,12 @@
 const products =[
     {id:1,name:"hello",price:34,status:"pending",qty:3},
-    {id:1,name:"hello 1",price:34,status:"pending",qty:5},
-    {id:1,name:"hello 2",price:34,status:"pending",qty:2}
+    {id:2,name:"hello 1",price:34,status:"pending",qty:5},
+    {id:3,name:"hello 2",price:34,status:"pending",qty:2}
 
 ];
 const newarray=products.map((item)=>({...item,price:item.price+5,status:item.status="complete",item_total:item.qty*item.price}))
 console.log(newarray)
+
+const updated=products.map((item)
 
 
