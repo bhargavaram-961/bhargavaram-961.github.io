351b6685f3c2072c897ff9977d7322eb5974c370
bhargavaram-961 <bhargavaram961@gmail.com>
2025-05-17 21:03:38 +0530

    u

--- a1/index16.js
+++ a1/index16.js
@@ -1,19 +1,48 @@
-function add(x, y) {
-    return new Promise((resolve, reject) => {
-        resolve(x + y);
-    });
-}
+// function add(x, y) {
+//     return new Promise((resolve, reject) => {
+//         resolve(x + y);
+//     });
+// }
 
-function sqr(a){
+// function sqr(a){
+//     return a*a
+// }
+// async function main() {
+//     try {
+//         let sum = await add(4, 5);
+//         let result = sqr(sum);
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+// main()
+function add(x,y)
+{
+    if(x<5)
+    {
+        return Promise.reject("less than 5");
+    }
+    else
+    {
+    return Promise.resolve(x+y)
+    }
+}
+function sqr(a)
+{
     return a*a
 }
-async function main() {
-    try {
-        let sum = await add(4, 5);
-        let result = sqr(sum);
-        console.log(result);
-    } catch (error) {
-        console.error(error);
+async function main()
+{
+    try
+    {
+    let sum=await add(4,5)
+    let res=sqr(sum)
+    console.log(res)
+    }
+    catch(error)
+    {
+        console.log(error);
     }
 }
 main()
