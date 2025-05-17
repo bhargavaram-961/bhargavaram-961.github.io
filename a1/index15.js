dafbcf52ed7e4bf5e2b2ffac9780f2e36d8735b1
bhargavaram-961 <bhargavaram961@gmail.com>
2025-05-17 20:33:26 +0530

    updated

---
+++ a1/index15.js
@@ -0,0 +1,14 @@
+async function greet(){
+  console.log("Hello World")
+}
+ 
+async function dispData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json()
+  console.log(data);
+}
+
+await dispData();
+greet()
+
+ 
